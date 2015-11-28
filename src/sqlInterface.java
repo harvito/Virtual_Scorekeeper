460783d7e42ae0887bd61c360334f172b61cdeb8
Loufe <loufeadmin@gmail.com>
2015-11-28 12:25:58 -0500

    modified the sqlinterface a little, cleaning it up

--- src/sqlInterface.java
+++ src/sqlInterface.java
@@ -1,4 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//import com.mysql.jdbc.Driver;
 
 public class sqlInterface {
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
 
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+			String connectionUrl = "jdbc:mysql://localhost:3306/testdatabase";
+			String connectionUser = "testuser";
+			String connectionPassword = "testpassword";
+			conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM employees");
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String firstName = rs.getString("first_name");
+				String lastName = rs.getString("last_name");
+				System.out.println("ID: " + id + ", First Name: " + firstName
+						+ ", Last Name: " + lastName);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (stmt != null) stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (conn != null) conn.close(); } catch (SQLException e) { e.printStackTrace(); }
+		}
+	}
 }
